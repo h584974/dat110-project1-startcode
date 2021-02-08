ca893231617a796009a41ff21b2a6ea88450e3bd
olive <olive@DESKTOP-R044BGP>
2021-02-08 01:26:22 +0100

    yikes

--- dat110-project1/src/no/hvl/dat110/rpc/RPCClient.java
+++ dat110-project1/src/no/hvl/dat110/rpc/RPCClient.java
@@ -19,37 +19,21 @@ public class RPCClient {
 	
 	public void connect() {
 		
-		// TODO: connect using the underlying messaging layer connection
-		
-	    throw new UnsupportedOperationException(TODO.method());
+		connection = msgclient.connect();
 			
 	}
 	
 	public void disconnect() {
 		
-		// TODO: disconnect/close the underlying messaging connection
-		
-		throw new UnsupportedOperationException(TODO.method());
+		connection = null;
 		
 	}
 	
 	public byte[] call(byte[] rpcrequest) {
 		
-		byte[] rpcreply;
-		
-		/* TODO: 
-		
-		Make a remote call on the RPC server by sending the RPC request message
-		and receive an RPC reply message
-		
-		rpcrequest is the marshalled rpcrequest from the client-stub
-		rpctreply is the rpcreply to be unmarshalled by the client-stub
-		
-		*/
-		
-		if (true) {
-			throw new UnsupportedOperationException(TODO.method());
-		}
+		Message msg = new Message(rpcrequest);
+		connection.send(msg);
+		byte[] rpcreply = connection.receive().getData();
 		
 		return rpcreply;
 		
